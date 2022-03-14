9e6bad3079b55a685dfb66aac2eccfebce7f4797
Jorge Silva <jorgesilva010995@gmail.com>
2022-03-14 11:04:45 -0300

    Configurado redes sociais, falta implantar links e imgs

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,16 @@ function Footer() {
   return (
     <footer>
       <div>
-          <img src={Logo} alt="RB-Moveis Planejados" />
+          <img className="Logo" src={Logo} alt="RB-Moveis Planejados" />
+          <ul className="SocialMedia">
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">WhatsApp</a></li>
+            <li><a href="#">Youtube</a></li>
+          </ul>
+      </div>
+      <div className="Direitos">
+        <h3>Todos os direitos reservados.</h3>
       </div>
     </footer>
   );
